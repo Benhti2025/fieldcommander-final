df453e64682ba46ad2917623cded6ff43577381a
Benhti2025 <divergentassets@outlook.com>
2025-07-26 12:41:54 -0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // Optional rewrites, uncomment if needed:
+  // async rewrites() {
+  //   return [
+  //     { source: "/auth", destination: "/auth" },
+  //     { source: "/logs", destination: "/logs" },
+  //   ];
+  // },
 };
 
 module.exports = nextConfig;
